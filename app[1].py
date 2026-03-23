a3772b642f2521e539e751e09f3227be58e0a312
gaddamgopal <164573739+gaddamgopal@users.noreply.github.com>
2026-03-23 20:59:15 +0530

    Add files via upload

---
+++ app[1].py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, jsonify
+import pandas as pd
+import random
+
+app = Flask(__name__)
+
+try:
+    data = pd.read_csv('traffic.csv')
+except:
+    data = None
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/get_data')
+def get_data():
+    if data is not None:
+        row = data.sample().iloc[0]
+        t1 = int(row[0])
+        t2 = int(row[1])
+    else:
+        t1 = random.randint(10, 100)
+        t2 = random.randint(10, 100)
+
+    return jsonify({'road1': t1, 'road2': t2})
+
+if __name__ == '__main__':
+    app.run(debug=True)
